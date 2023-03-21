0c00a2f160e12ec319649ac8ca08af932b6ec285
junerosee <junechege1@gmail.com>
2023-03-21 01:32:53 -0700

    Prints single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	for (y = '0'; y <= '9'; y++)
+	putchar(y);
+	putchar('\n');
+	return (0);
+}
