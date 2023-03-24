8eeb30e5c54a8cc45922bd7e19e50d7c394ae221
junerosee <junechege1@gmail.com>
2023-03-24 12:40:20 -0700

    Draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times _ should be printed
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
