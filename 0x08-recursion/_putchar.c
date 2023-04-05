f560f513ec7b77193084ec116c47ac2071f0e837
junerosee <junechege1@gmail.com>
2023-04-05 10:30:59 -0700

    Prints string in reverse

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * _putchar - writes character c to stdout
- * @c: character to be printed  out
+ * _putchar - prints out character c to stdout
+ * @c: character to be printed out
  * Return: On success 1
  */
 int _putchar(char c)
