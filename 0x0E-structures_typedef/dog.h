ea3a2bf217f5323949facc4d654e58a6733e0d58
junerosee <junechege1@gmail.com>
2023-04-18 09:43:40 -0700

    Defines a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ * Return: Always 0.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
